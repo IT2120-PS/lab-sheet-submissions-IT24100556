7273a28d9989358647da0a3348d65d3c34868ccd
IT24100556 <it24100556@my.sliit.lk>
2025-09-02 18:06:46 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24100556_PS_lab_6.R
@@ -0,0 +1,27 @@
+setwd("C:\\Users\\user\\Desktop\\IT24100556_PS_Lab_6")
+
+dbinom(40,44,0.92)
+ 
+pbinom(35,44,0.92,lower.tail=TRUE)
+
+1-pbinom(37,44,0.92,lower.tail=TRUE)
+pbinom(37,44,0.92,lower.tail=FALSE)
+
+pbinom(42,44,0.92,lower.tail=TRUE)-pbinom(39,44,0.92,lower.tail=TRUE)
+
+
+dpois(6,5)
+
+ppois(6,5,lower.tail = FALSE)
+
+##exercise
+## Q1 (ii)
+1 - pbinom(46, 50, 0.85, lower.tail = TRUE)
+
+##Q2 (iii)
+dpois(15, 12)
+
+
+
+
+
